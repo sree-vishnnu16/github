7b965c774da54283499976806992520a69da9182
Sree Vishnu <gentleman.sree@gmail.com>
2025-07-15 17:55:47 +0530

    Logout function changes

--- lib/feature/login/infrastructure/data/remote/login_remote_service.dart
+++ lib/feature/login/infrastructure/data/remote/login_remote_service.dart
@@ -1,4 +1,5 @@
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class LoginRemoteService {
   Future<UserCredential?> signInWithGithub() async {
@@ -14,13 +15,34 @@ class LoginRemoteService {
     }
   }
 
+  // Future<bool> signOut() async {
+  //   try {
+  //      await FirebaseAuth.instance.signOut();
+  //      return true;
+  //   } on FirebaseAuthException catch (e) {
+  //     rethrow;
+  //   } catch (e) {
+  //     rethrow;
+  //   }
+  // }
+
   Future<bool> signOut() async {
     try {
-       await FirebaseAuth.instance.signOut();
-       return true;
+      await FirebaseAuth.instance.signOut();
+
+      // Optional: Attempt to log out from GitHub in browser
+      // This won't revoke tokens but will clear session cookies
+      await launchUrl(
+        Uri.parse("https://github.com/logout"),
+        mode: LaunchMode.externalApplication,
+      );
+
+      return true;
     } on FirebaseAuthException catch (e) {
+      print('FirebaseAuthException on logout: ${e.code}');
       rethrow;
     } catch (e) {
+      print('Unknown error during logout: $e');
       rethrow;
     }
   }
